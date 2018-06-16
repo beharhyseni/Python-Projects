f088fdd05dc48ad61af3753e679a64f87110976b
beharh <behar.hyseni@alumni.ubc.ca>
2018-06-15 20:31:24 -0700

    simple english dictionary (not robust yet)

--- Dictionary Application/Dict_App.py
+++ Dictionary Application/Dict_App.py
@@ -3,4 +3,9 @@ import json
 
 dict_data = json.load(open("data.json"))
 
-print(dict_data["rain"][0])
+word = input("Enter the word you would like to translate: ")
+
+if word in dict_data.keys():
+    print(dict_data[word][0])
+else:
+    print("Word does not exist. Please try again with a different word.")
