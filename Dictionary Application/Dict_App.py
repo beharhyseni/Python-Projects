e11f2378b33030f4594feaf58248666f65a5b163
beharh <behar.hyseni@alumni.ubc.ca>
2018-06-15 21:54:14 -0700

    Simple English Dictionary - Dict_App 1.0

--- Dictionary Application/Dict_App.py
+++ Dictionary Application/Dict_App.py
@@ -3,20 +3,27 @@ from difflib import get_close_matches
 
 dict_data = json.load(open("data.json"))
 
-word = input("Enter the word you would like to translate: \n")
+word = input("Enter the word you would like to translate: \n").lower()
 
-if word in dict_data.keys():
-    print(dict_data[word][0])
+if word.lower() in dict_data.keys():
+    count = 0
+    for w in dict_data[word.lower()]:
+        print("Definition %s: %s" % (count+1, dict_data[word.lower()][count]))
+        count += 1
+elif word.upper() in dict_data.keys():
+    count = 0
+    print("Definition %s: %s" % (count + 1, dict_data[word.upper()][count]))
+    count += 1
 elif get_close_matches(word, dict_data.keys()):
-    close_match = get_close_matches(word,dict_data.keys())[0]
-    match_word = input("Did you mean '%s' instead?:    Enter Y if yes, and N if no. \n" % close_match)
+    close_match = get_close_matches(word, dict_data.keys())[0]
+    match_word = input("Did you mean '%s' instead?:    Enter Y if yes, and N if no. \n" % close_match).lower()
 
-    if match_word == "Y":
-       count = 1
-       for w in dict_data[close_match]:
-           print("Definition %d: %s" % (count, w))
-           count+=1
-    elif match_word == "N":
+    if match_word == "y":
+        count = 1
+        for w in dict_data[close_match]:
+            print("Definition %d: %s" % (count, w))
+            count += 1
+    elif match_word == "n":
         print("Word does not exist! - Please try again with a different word.")
     else:
         print("Word does not exist! - Please try again with a different word.")
