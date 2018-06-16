5d8098a29e9621822f0f47c6f47a7e450b2c7a90
beharh <behar.hyseni@alumni.ubc.ca>
2018-06-15 21:24:31 -0700

    Added error handling functionality

--- Dictionary Application/Dict_App.py
+++ Dictionary Application/Dict_App.py
@@ -1,11 +1,25 @@
 import json
-
+from difflib import get_close_matches
 
 dict_data = json.load(open("data.json"))
 
-word = input("Enter the word you would like to translate: ")
+word = input("Enter the word you would like to translate: \n")
 
 if word in dict_data.keys():
     print(dict_data[word][0])
+elif get_close_matches(word, dict_data.keys()):
+    close_match = get_close_matches(word,dict_data.keys())[0]
+    match_word = input("Did you mean '%s' instead?:    Enter Y if yes, and N if no. \n" % close_match)
+
+    if match_word == "Y":
+       count = 1
+       for w in dict_data[close_match]:
+           print("Definition %d: %s" % (count, w))
+           count+=1
+    elif match_word == "N":
+        print("Word does not exist! - Please try again with a different word.")
+    else:
+        print("Word does not exist! - Please try again with a different word.")
+
 else:
-    print("Word does not exist. Please try again with a different word.")
+    print("Word does not exist! - Please try again with a different word.")
